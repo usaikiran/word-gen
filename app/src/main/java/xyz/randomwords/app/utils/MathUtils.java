d224ac7849e5c4436943d6e80a8a18a66208c77d
botsk <usaikiran89@gmail.com>
2019-06-29 20:42:52 +0530

    removed redundant files

--- app/src/main/java/xyz/randomwords/app/utils/MathUtils.java
+++
@@ -1,8 +0,0 @@
-package xyz.randomwords.app.utils;
-
-public class MathUtils {
-
-    public static int random( int minInclusive, int maxInclusive ){
-        return minInclusive + (int)( Math.random() * ( maxInclusive - minInclusive ) );
-    }
-}
